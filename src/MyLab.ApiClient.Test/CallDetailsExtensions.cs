8c2eef0e5f10fdd75df288d4e45e2b3f3b6f1b91
Andrey O <ozzy@triasoft.com>
2020-11-11 02:07:11 +0300

    Update ToTestDump methods

--- src/MyLab.ApiClient.Test/CallDetailsExtensions.cs
+++ src/MyLab.ApiClient.Test/CallDetailsExtensions.cs
@@ -12,16 +12,16 @@ namespace MyLab.ApiClient.Test
         /// Gets web-call dump for test output 
         /// </summary>
         /// <typeparam name="TRes">call result</typeparam>
-        /// <typeparam name="TContract">API contract</typeparam>
         /// <param name="call">web call</param>
+        /// <param name="contractType">API contract</param>
         /// <returns>string dump</returns>
-        public static string ToTestDump<TRes, TContract>(this CallDetails<TRes> call)
+        public static string ToTestDump<TRes>(this CallDetails<TRes> call, Type contractType = null)
         {
             if (call == null) throw new ArgumentNullException(nameof(call));
 
             var sb = new StringBuilder();
 
-            WriteToString<TRes, TContract>(call, sb);
+            WriteToString(call, sb, contractType);
             
             return sb.ToString();
         }
@@ -33,18 +33,44 @@ namespace MyLab.ApiClient.Test
         /// <typeparam name="TContract">API contract</typeparam>
         /// <param name="call">web call</param>
         /// <returns>string dump</returns>
+        public static string ToTestDump<TRes, TContract>(this CallDetails<TRes> call)
+        {
+            return ToTestDump(call, typeof(TContract));
+        }
+
+        /// <summary>
+        /// Gets web-call dump for test output 
+        /// </summary>
+        /// <typeparam name="TRes">call result</typeparam>
+        /// <typeparam name="TContract">API contract</typeparam>
+        /// <param name="call">web call</param>
+        /// <returns>string dump</returns>
         public static string ToTestDump<TRes, TContract>(this TestCallDetails<TRes> call)
+        {
+            return ToTestDump(call, typeof(TContract));
+        }
+
+        /// <summary>
+        /// Gets web-call dump for test output 
+        /// </summary>
+        /// <typeparam name="TRes">call result</typeparam>
+        /// <param name="contractType">API contract</param>
+        /// <param name="call">web call</param>
+        /// <returns>string dump</returns>
+        public static string ToTestDump<TRes>(this TestCallDetails<TRes> call, Type contractType = null)
         {
             if (call == null) throw new ArgumentNullException(nameof(call));
 
             var sb = new StringBuilder();
 
-            WriteToString<TRes, TContract>(call, sb);
+            WriteToString<TRes>(call, sb, contractType);
 
             if (call.ResponseProcessingError)
             {
+                var contractName = GetContractName(contractType);
+
                 sb.AppendLine("");
-                sb.AppendLine($"===== RESPONSE PROC ERROR ({typeof(TContract).Name}) =====");
+                sb.AppendLine($"===== RESPONSE PROC ERROR ({contractName}) =====");
                 sb.AppendLine("");
                 sb.AppendLine(call.ProcessingError.ToString());
                 sb.AppendLine("===== RESPONSE PROC ERROR END =====");
@@ -53,18 +79,24 @@ namespace MyLab.ApiClient.Test
             return sb.ToString();
         }
 
-        static void WriteToString<TRes, TContract>(CallDetails<TRes> call, StringBuilder sb)
+        static void WriteToString<TRes>(CallDetails<TRes> call, StringBuilder sb, Type contractType)
         {
+            var contractName = GetContractName(contractType);
+
             sb.AppendLine("");
-            sb.AppendLine($"===== REQUEST BEGIN ({typeof(TContract).Name}) =====");
+            sb.AppendLine($"===== REQUEST BEGIN ({contractName}) =====");
             sb.AppendLine("");
             sb.AppendLine(call.RequestDump);
             sb.AppendLine("===== REQUEST END =====");
             sb.AppendLine("");
-            sb.AppendLine($"===== RESPONSE BEGIN ({typeof(TContract).Name}) =====");
+            sb.AppendLine($"===== RESPONSE BEGIN ({contractName}) =====");
             sb.AppendLine("");
             sb.AppendLine(call.ResponseDump);
             sb.AppendLine("===== RESPONSE END =====");
         }
+
+        static string GetContractName(Type contractType) => contractType != null
+            ? contractType.Name
+            : "[undefined contract]";
     }
 }

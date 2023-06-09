927adebcc1a999b1faf20e414b6b9a08124a7ad0
Andrey O <ozzy@triasoft.com>
2023-06-09 14:38:23 +0300

    Introduce TestApiFixture

---
+++ src/FuncTest/TestApiFixtureBehavior.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MyLab.ApiClient.Test;
+using TestServer;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FuncTest
+{
+    public class TestApiFixtureBehavior : IClassFixture<TestApiFixture<Startup, ITestApi>>
+    {
+        private readonly TestApiFixture<Startup, ITestApi> _fxt;
+
+        public TestApiFixtureBehavior(TestApiFixture<Startup, ITestApi> fxt, ITestOutputHelper output)
+        {
+            fxt.Output = output;
+            _fxt = fxt;
+        }
+
+        [Fact]
+        public async Task ShouldInvokeServerCall()
+        {
+            //Arrange 
+            var client = _fxt.Start().ApiClient;
+
+            //Act
+            var val = await client.Call(service => service.AddSalt("test"));
+
+            //Assert
+            Assert.Equal("test-foo", val.ResponseContent);
+        }
+
+        [Fact]
+        public async Task ShouldInvokeServerCallWithProxy()
+        {
+            //Arrange 
+            var client = _fxt.StartWithProxy().ApiClient;
+
+            //Act
+            var val = await client.AddSalt("test");
+
+            //Assert
+            Assert.Equal("test-foo", val);
+        }
+
+        [Fact]
+        public async Task ShouldInvokeWithServiceConfiguration()
+        {
+            //Arrange
+            var client = _fxt.Start(srv => 
+                    srv.AddSingleton(new StringProcessorService("bar"))
+                ).ApiClient;
+
+            //Act
+            var val = await client.Call(s => s.AddSalt("test"));
+
+            //Assert
+            Assert.Equal("test-bar", val.ResponseContent);
+        }
+
+        [Fact]
+        public async Task ShouldInvokeWithHttpModifying()
+        {
+            //Arrange
+            var client = _fxt.Start(httpClientTuner: c => 
+                    c.DefaultRequestHeaders.Add("ArgumentHeader", "test")
+                ).ApiClient;
+
+            //Act
+            var val = await client.Call(s => s.AddSaltToHeader());
+
+            //Assert
+            Assert.Equal("test-foo", val.ResponseContent);
+        }
+    }
+}
---
+++ src/MyLab.ApiClient.Test/TestApiFixture.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace MyLab.ApiClient.Test
+{
+    /// <summary>
+    /// Provides tools to create test API application
+    /// </summary>
+    public class TestApiFixture<TStartup, TApiContact> : IDisposable
+        where TStartup : class
+        where TApiContact : class
+    {
+        private readonly WebApplicationFactory<TStartup> _appFactory;
+
+        /// <summary>
+        /// Overrides services
+        /// </summary>
+        public Action<IServiceCollection> ServiceOverrider { get; set; }
+
+        /// <summary>
+        /// Additional tunes <see cref="HttpClient"/>
+        /// </summary>
+        public Action<HttpClient> HttpClientTuner { get; set; }
+        
+        /// <summary>
+        /// Test output for log writing
+        /// </summary>
+        public ITestOutputHelper Output { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="TestApiFixture{TStartup, TApiContract}"/>
+        /// </summary>
+        public TestApiFixture()
+        {
+            _appFactory = new WebApplicationFactory<TStartup>();
+        }
+
+        /// <summary>
+        /// Starts test API instance and returns <see cref="ApiClient{TApiContract}"/>
+        /// </summary>
+        public ClientTestApiSet<TApiContact> Start(
+            Action<IServiceCollection> serviceOverrider = null, 
+            Action<HttpClient> httpClientTuner = null)
+        {
+            return Start(out _, serviceOverrider, httpClientTuner);
+        }
+
+        /// <summary>
+        /// Starts test API instance and returns <see cref="ApiClient{TApiContract}"/>
+        /// </summary>
+        public ClientTestApiSet<TApiContact> Start(out HttpClient innerHttpClient,
+            Action<IServiceCollection> serviceOverrider = null,
+            Action<HttpClient> httpClientTuner = null)
+        {
+            var factory = _appFactory.WithWebHostBuilder(builder => builder.ConfigureTestServices(srv =>
+            {
+                ServiceOverrider?.Invoke(srv);
+                serviceOverrider?.Invoke(srv);
+            }));
+
+            var opt = new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            };
+
+            innerHttpClient = factory.CreateClient(opt);
+
+            HttpClientTuner?.Invoke(innerHttpClient);
+            httpClientTuner?.Invoke(innerHttpClient);
+
+            var client = new ApiClient<TApiContact>(new SingleHttpClientProvider(innerHttpClient));
+
+            return new ClientTestApiSet<TApiContact>
+            {
+                HttpClient = innerHttpClient,
+                ApiClient = new TestApiClient<TApiContact>(client)
+                {
+                    Output = Output
+                },
+                ServiceProvider = factory.Services
+            };
+        }
+
+        /// <summary>
+        /// Starts test API instance and returns <see cref="ApiClient{TApiContract}"/>
+        /// </summary>
+        public ProxyTestApiSet<TApiContact> StartWithProxy(
+            Action<IServiceCollection> serviceOverrider = null,
+            Action<HttpClient> httpClientTuner = null)
+        {
+            return StartWithProxy(out _, serviceOverrider, httpClientTuner);
+        }
+
+        /// <summary>
+        /// Starts test API instance and returns <see cref="ApiClient{TApiContract}"/>
+        /// </summary>
+        public ProxyTestApiSet<TApiContact> StartWithProxy(out HttpClient innerHttpClient,
+            Action<IServiceCollection> serviceOverrider = null,
+            Action<HttpClient> httpClientTuner = null)
+        {
+            var factory = _appFactory.WithWebHostBuilder(builder => builder.ConfigureTestServices(srv =>
+            {
+                ServiceOverrider?.Invoke(srv);
+                serviceOverrider?.Invoke(srv);
+            }));
+
+            var opt = new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            };
+
+            innerHttpClient = factory.CreateClient(opt);
+
+            HttpClientTuner?.Invoke(innerHttpClient);
+            httpClientTuner?.Invoke(innerHttpClient);
+
+            var proxy = ApiProxy<TApiContact>.Create(
+                new SingleHttpClientProvider(innerHttpClient), 
+                null,
+                new TestOutputApiCallObserver(Output, typeof(TApiContact)));
+
+            return new ProxyTestApiSet<TApiContact>
+            {
+                ApiClient = proxy,
+                HttpClient = innerHttpClient,
+                ServiceProvider = factory.Services
+            };
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _appFactory?.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// Contains tools to operate with test API
+    /// </summary>
+    public class TestApiSet
+    {
+        /// <summary>
+        /// Application service provider
+        /// </summary>
+        public IServiceProvider ServiceProvider { get; set; }
+        /// <summary>
+        /// An <see cref="HttpClient"/> which will be used for interaction with API
+        /// </summary>
+        public HttpClient HttpClient { get; set; }
+    }
+
+    /// <summary>
+    /// Contains tst API tools and <see cref="TestApiClient{TApiContact}"/> intended for interaction with API
+    /// </summary>
+    /// <typeparam name="TContract">API contract</typeparam>
+    public class ClientTestApiSet<TContract> : TestApiSet
+        where TContract : class
+    {
+        /// <summary>
+        /// A client intended for interaction with API
+        /// </summary>
+        public TestApiClient<TContract> ApiClient { get; set; }
+    }
+
+    /// <summary>
+    /// Contains tst API tools and proxy intended for interaction with API
+    /// </summary>
+    /// <typeparam name="TContract">API contract</typeparam>
+    public class ProxyTestApiSet<TContract> : TestApiSet
+        where TContract : class
+    {
+        /// <summary>
+        /// A client intended for interaction with API
+        /// </summary>
+        public TContract ApiClient { get; set; }
+    }
+}
